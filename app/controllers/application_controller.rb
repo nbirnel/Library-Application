5d6f42bab2c776c9e6f46286b586c163c33d91d2
Noah Birnel <nbirnel@gmail.com>
2014-08-25 22:59:55 -0700

    refactor a bit of application controller
    This improves flog and flay scores a bit. I will refrain from a PR until
    near the end if it looks like we need it. I also have yet to see how
    Code Climate feels about it. One line for json is not tested.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   def check_valid_user
       unless current_user
         redirect_to :back, notice: "You must login to or sign up to user this feature"
@@ -9,15 +10,14 @@ class ApplicationController < ActionController::Base
       end
   end
 
-
-def create
+  def create
     # The subclasses need to set @item 
     # They may set @redirect if it is not the same as @item
     @redirect ||= @item 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @redirect, notice: "#{@item.class.to_s} was successfully created." }
-        format.json { render :show, status: :created, location: @redirect }
+        format.html { html_success_says 'created' }
+        format.json { json_success_says :created }
       else
         format.html { render :new }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -32,8 +32,8 @@ def create
     
     respond_to do |format|
       if @item.update(@current_parameters)
-        format.html { redirect_to @redirect, notice: "#{@item.class.to_s} was successfully updated." }
-        format.json { render :show, status: :ok, location: @redirect }
+        format.html { html_success_says 'updated' }
+        format.json { json_success_says :ok }
       else
         format.html { render :edit }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -43,6 +43,16 @@ def create
 
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  private
+
+  def html_success_says action
+    redirect_to @redirect, notice: "#{@item.class.to_s} was successfully #{action}."
+  end
+
+  def json_success_says status
+    render :show, status: status, location: @redirect
   end
 
 end
